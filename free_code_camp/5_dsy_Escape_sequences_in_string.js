a49959b0d87556c0d5a5348764fa834ca842d8c6
Daniel <ostrov2ostrov2@gmail.com>
2022-12-06 13:29:01 +0300

    escape_sequences

---
+++ free_code_camp/5_dsy_Escape_sequences_in_string.js
@@ -0,0 +1,14 @@
+// Code	Output
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \t	tab
+// \r	carriage return
+// \b	word boundary
+// \f	form feed
+// // Note that the backslash itself must be escaped in order to display as a backslash.
+
+
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
