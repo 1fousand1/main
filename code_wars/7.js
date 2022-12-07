6ecc16a4595bd94afc67631b9fb0fa193d2df239
Daniel <ostrov2ostrov2@gmail.com>
2022-12-07 14:34:15 +0300

    New day - new function
    Функция возвращает число с противоположенным знаком

---
+++ code_wars/7.js
@@ -0,0 +1,7 @@
+function opposite(number) {
+    if (number < 0)  return Math.abs(number);
+    else  return number * -1;
+  }
+
+
+  ////нужно превратить отрицательное число в положительно, а положительное в отрицательное
