fb5e540ef4098ad4a850139e4a8f95e6855bb9c2
Daniel <ostrov2ostrov2@gmail.com>
2022-12-08 16:59:44 +0300

    New func
    Функция возвращает сумму квадратов соседних чисел

---
+++ code_wars/8.js
@@ -0,0 +1,9 @@
+function squareSum(numbers){
+    var result = 0;
+    numbers.forEach(function(item) {
+        result += Math.pow(item, 2);
+    });
+    return result;
+}
+
+///Возвращает квадратную сумму
