d593da2425b1ae744d5d5b816346b6f1afb41c3a
Daniel <ostrov2ostrov2@gmail.com>
2022-12-14 05:36:22 +0300

    new func

--- code_wars/9.js
+++ code_wars/9.js
@@ -36,3 +36,21 @@ var numb = number;
     } 
 }
 // цифры от 1 до 9 превращает в строку "one", "two".."nine"
+function arrayPlusArray(arr1, arr2) {
+    var x = [].concat(arr1, arr2)
+    var s = 0;
+    for (i = 0; i < x.length; i++){
+    s += x[i];
+    }
+    return s
+}
+// получить сумму двух массивов
+
+
+function sumStr(a,b) {
+    let n1 = Number(a);
+    let n2 = Number(b);
+    let sum = n1 + n2;
+    return String(sum)
+  }
+// трансформирует строки в числа, суммирует их и сумму трансформирует обрано в строку  
