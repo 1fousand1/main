bc33e3416987ed9f97da33dd1bb65f11fc611e08
Daniel <ostrov2ostrov2@gmail.com>
2022-12-14 05:00:11 +0300

    New file - new func

---
+++ code_wars/9.js
@@ -0,0 +1,38 @@
+function switchItUp(number){   
+var numb = number;
+    switch (numb === number) {
+        case number===1:
+            return 'One';
+            break;
+        case number===2:
+            return 'Two';
+            break;
+        case number===3:
+            return 'Three';
+            break;
+        case number===4:
+            return 'Four';
+            break;
+        case number===5:
+            return 'Five';
+            break;
+        case number===6:
+            return 'Six';
+            break;
+        case number===7:
+            return 'Seven';
+            break;
+        case number===8:
+            return 'Eight';
+            break;
+        case number===9:
+            return 'Nine';
+            break;
+        case number===0:
+            return 'Zero';
+            break;
+        default:
+            break;
+    } 
+}
+// цифры от 1 до 9 превращает в строку "one", "two".."nine"
